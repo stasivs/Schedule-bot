09f6e59ee4fa9e56ed59840089aeb3d1459875a6
stasivs <mr.stasivs@yandex.ru>
2020-12-12 01:09:35 +0300

    Added registration, schedule sender, handled errors in user input

--- Bot.py
+++ Bot.py
@@ -1,92 +1,114 @@
-#допилить другой язык, таймер, ответы на неверно введеный текст, ссылки на дистант, сброс всех параметров, плашечку с информацией
 from telegram.ext import Updater, MessageHandler, CommandHandler, Filters, Dispatcher, ConversationHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 import json
-import requests
-import Converter
 
 with open('data.json') as json_file:
-    data = json.load(json_file)
+    schedules = json.load(json_file)
 
 
-#class Bot:
+class Bot:
     def __init__(self):
-        pass
+        self.weekend = ["понедельник", "вторник", "среда", "четверг", "пятница"]
+        self.pair_time = ["8:30 - 10:00", "10:10 - 11:40", "11:50 - 13:20", "13:30 - 15:00", "15:10 - 16:40", "16:50 - 18:20"]
+        self.parity = ["ч", "н"]
+        with open("db.json", "r", encoding="utf8") as db:
+            self.data = json.load(db)
 
-    def start(self, update):
-        update.message.reply_text("ку заведомо знаем, что с ИСА скажи свой курс(хочешь закончить напиши /stop)")
+    def start(self, bot, update):
+        update.message.reply_text("Добро пожаловать в бота для выдачи рассписания,\n"
+                                  "Если вы еще не регистрировались используйте комманду\n"
+                                  "/reg, а если нет, то чтобы получить расписание на день, то\n"
+                                  "Введите в формате 'День недели Н/Ч'")
+
+    def registration_start(self, bot, update):
+        update.message.reply_text("Привет! Укажи свой институт, курс, группу, подгруппу, в формате\n"
+                                  "Институт курс-группа подгруппа\n"
+                                  "Пример:\n"
+                                  "ИФО 1-1 1")
         return 1
 
-    def first(self, update, user_data):
-        update.message.reply_text('Хорошо, напиши свою группу')
-        course = update.message.text
-        user_data['course'] = course
-        return 2
+    def registration_end(self, bot, update):
+        text = update.message.text.split()
+        # Проверка на формат
+        try:
+            institute, course, group, subgroup = text[0], *text[1].split("-"), text[2]
+        except Exception:
+            update.message.reply_text("Неправильный формат данных")
+            return 1
 
-    def second(self, update, user_data):
-        update.message.reply_text('Хорошо, напиши свою подгруппу')
-        group = update.message.text
-        user_data['group'] = group
-        return 3
+        key = institute.lower() + " " + course + " " + group
+        data = {"institute": institute, "course": course, "group": group, "subgroup": subgroup}
 
-    def third(self, update, user_data):
-        update.message.reply_text('Хорошо, напиши нужный день недели')
-        undergroup = update.message.text
-        user_data['undergroup'] = undergroup
-        return 4
+        # Проверка на корректность
+        if key in schedules and subgroup in ["1", "2"]:
+            self.data[str(update.message.chat.id)] = data
+            with open("db.json", "w", encoding="utf8") as db:
+                json.dump(self.data, db)
 
-    def fourth(self, update, user_data):
-        update.message.reply_text('Хорошо, напиши четность недели, где 0 - нечетная, 1 - четная')
-        dayofweek = update.message.text
-        return 5
+            update.message.reply_text("Регистрация успешно пройдена")
 
-    def five(self, update):
-        update.message.reply_text('Держи твое расписание! Ждем снова...')
-        parity = update.message.text
-        print(data['ИСА ' + course + group + ' (Б) (оч.ф.о., 08.03.01 )'][undergroup][dayofweek][parity])
+            return ConversationHandler.END
+        elif subgroup not in ["1", "2"] and key not in schedules:
+            update.message.reply_text("У нас отсутствует информация о таком направлении в бд\n"
+                                      "Неправильно введена подгруппа (1 или 2)")
+        elif key not in schedules:
+            update.message.reply_text("У нас отсутствует информация о таком направлении в бд")
+        elif subgroup not in ["1", "2"]:
+            update.message.reply_text("Неправильно введена подгруппа (1 или 2)")
+        return 1
+
+    def stop(self, bot, update):
+        update.message.reply_text('Регистрация прервана')
         return ConversationHandler.END
 
-# для остановки конверсейшн бота
-    def stop(self, update):
-        update.message.reply_text('Был рад помочь!')
-        return ConversationHandler.END
+    def schedule(self, bot, update):
+        id = str(update.message.chat.id)
+        if id in self.data:
+            text = update.message.text.lower().split()
+            # Проверка на формат
+            if text[0] in self.weekend and text[1] in self.parity:
+                user_data = self.data[id]
+                # Ключ / Подгруппа
+                key, subgroup = user_data["institute"].lower() + " " + user_data["course"] + " " + user_data["group"], int(user_data["subgroup"]) - 1
+                # День / Четность
+                day, parity = text[0].capitalize(), self.parity.index(text[1])
+                # Итоговое расписание
+                schedule = schedules[key][subgroup][day][parity]
+
+                # Итоговый формат расписания
+                res = ""
+                update.message.reply_text("Вот ваше расписание:")
+                for number, lesson in enumerate(schedule):
+                    if not lesson:
+                        lesson = "Ничего"
+                    res += "{} Пара {}:\n{}\n\n".format(number + 1, self.pair_time[number], lesson.replace("\n", " "))
+                update.message.reply_text(res)
+            else:
+                update.message.reply_text("Была допущена ошибка")
+        else:
+            update.message.reply_text("Сначала вам необходимо пройти регистрацию.\n"
+                                      "Выполните /reg")
 
 
 if __name__ == "__main__":
-    token = ""  # bot token
-    updater = Updater('token')
+    token = "1313546180:AAFhVROyewnsp2FjXZQO7GuMFexs1J_DQ0o"
+    updater = Updater(token)
     dp = updater.dispatcher
 
-    #bot = Bot()
+    bot = Bot()
 
-
-# сам конверсейшн с ботом
-conersation_handler = ConversationHandler(
-    entry_points=[CommandHandler('start', start)],
-    states={
-        1: [MessageHandler(Filters.all, first)],
-        2: [MessageHandler(Filters.all, second)],
-        3: [MessageHandler(Filters.all, third)],
-        4: [MessageHandler(Filters.all, fourth)],
-        5: [MessageHandler(Filters.all, five)],
-
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('reg', bot.registration_start)],
+        states={
+            1: [MessageHandler(Filters.text, bot.registration_end)],
         },
+        fallbacks=[CommandHandler('stop', bot.stop)]
+    )
 
-    fallbacks=[CommandHandler('stop', stop)]
-)
+    dp.add_handler(conv_handler)
+    dp.add_handler(CommandHandler('start', bot.start))
+    dp.add_handler(MessageHandler(Filters.text, bot.schedule))
 
-
-dp.add_handler(conv_handler)
-dp.add_handler(CommandHandler('stop', stop))
-dp.add_handler(CommandHandler('start', start))
-
-
-updater.start_polling()
-
-updater.idle()
-
-
-dp = updater.dispatcher
-
-
-
+    updater.start_polling()
+    print("Started")
+    updater.idle()
